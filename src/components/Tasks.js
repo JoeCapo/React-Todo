8bb8585b6d8d67b60eb5bf9c57943b2c641c9f04
JoeCapo <JoeCapo>
2021-08-04 20:44:13 -0400

    Add Components

---
+++ src/components/Tasks.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const tasks =[{
+    id: 1,
+    title: "Test Title",
+    isDeleted: false
+},
+{
+    id: 2,
+    title: "Test Task 2",
+    isDeleted: false
+}
+]
+
+
+const Tasks = () =>{
+    return(
+        <div>
+            { tasks.map(task => <p>Task ID: { task.id } Title: { task.title }</p>) }
+        </div>
+    )
+}
+
+export default Tasks;
---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import Tasks from './Tasks';
+const React = require("react");
+
+
+const Todo = () =>{
+    return(
+        <div>
+            <Tasks />
+        </div>
+    );
+}
+
+export default Todo;
